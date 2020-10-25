1a0a4733f6b393d4e4b277e25c41b176fa7ae7e9
Mark Schloeman <markofwisdumb@gmail.com>
2020-10-25 12:05:36 -0400

    Used black to make it prettier

--- daybuilder/widgets/stats_view.py
+++ daybuilder/widgets/stats_view.py
@@ -4,7 +4,26 @@ from daybuilder.utils import db_interface, stats, util
 import datetime
 from PyQt5.QtCore import Qt, QDate
 from PyQt5.QtGui import QBrush, QPalette, QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QDateEdit, QGridLayout, QPushButton, QVBoxLayout, QApplication, QStyleOption, QStyle, QComboBox, QCalendarWidget, QRadioButton, QStackedLayout, QCheckBox, QTextEdit, QSizePolicy
+from PyQt5.QtWidgets import (
+    QWidget,
+    QLabel,
+    QVBoxLayout,
+    QHBoxLayout,
+    QDateEdit,
+    QGridLayout,
+    QPushButton,
+    QVBoxLayout,
+    QApplication,
+    QStyleOption,
+    QStyle,
+    QComboBox,
+    QCalendarWidget,
+    QRadioButton,
+    QStackedLayout,
+    QCheckBox,
+    QTextEdit,
+    QSizePolicy,
+)
 import sqlite3
 import sys
 
@@ -17,9 +36,11 @@ class BarGraph(QWidget):
         self.graph_container = QWidget()
         self.graph = QVBoxLayout(self.graph_container)
 
+
 class StatsView(QWidget):
     """Widget that reads the daily planner database
     and highlights interesting correlations"""
+
     def __init__(self, db, *args, **kwargs):
         super(StatsView, self).__init__(*args, **kwargs)
         self.db = db
@@ -35,9 +56,6 @@ class StatsView(QWidget):
         mainlabel = QLabel("My Stats")
         mainlabel.setProperty("font-class", "title")
 
-        # START FROM SCRATCH. MAKE THE DRAFT FROM THE WHITEBOARD
-        # DO ALL OF THE SIMPLEST LABEL STUFF FIRST.
-        # LAST THING TO DO IS THE DIY-BAR GRAPH. MAKE A CUSTOM WIDGET FOR THAT.
         # Overall General Stats
         o_avg_rating = QLabel("Average Rating")
         o_avg_rating.setProperty("font-class", "heading")
@@ -49,11 +67,13 @@ class StatsView(QWidget):
         self.overall_completion.setProperty("font-class", "content")
 
         self.grid.addWidget(mainlabel, 0, 0, 1, 5)
-        #self.grid.addWidget(overall_label, 1, 0, alignment=Qt.AlignLeft)
+        # self.grid.addWidget(overall_label, 1, 0, alignment=Qt.AlignLeft)
         self.grid.addWidget(o_avg_rating, 2, 0, 1, 2, alignment=Qt.AlignCenter)
         self.grid.addWidget(o_task_completion, 2, 3, 1, 2, alignment=Qt.AlignCenter)
         self.grid.addWidget(self.overall_rating, 3, 0, 1, 2, alignment=Qt.AlignCenter)
-        self.grid.addWidget(self.overall_completion, 3, 3, 1, 2, alignment=Qt.AlignCenter)
+        self.grid.addWidget(
+            self.overall_completion, 3, 3, 1, 2, alignment=Qt.AlignCenter
+        )
 
         # Detailed Task Stats
         task_label = QLabel("tasks")
@@ -72,7 +92,7 @@ class StatsView(QWidget):
         self.least_completed = QLabel()
         self.least_completed.setProperty("font-class", "subcontent")
 
-        #self.grid.addWidget(task_label, 4, 0, alignment=Qt.AlignLeft)
+        # self.grid.addWidget(task_label, 4, 0, alignment=Qt.AlignLeft)
         self.grid.addWidget(most_common_task, 5, 0, 1, 2)
         self.grid.addWidget(self.most_common, 5, 2, 1, 2)
         self.grid.addWidget(most_completed_task, 6, 0, 1, 2)
@@ -98,8 +118,6 @@ class StatsView(QWidget):
     def get_data(self):
         self.ratings, self.items = stats.load_data(self.db)
 
-
-
     def display_overall_stats(self):
         avg_rating = self.ratings.rating.mean()
         # TODO : can make this more descriptive by
@@ -109,25 +127,33 @@ class StatsView(QWidget):
         self.overall_rating.setText(f"{avg_rating:.2} ({avg_rating_word})")
         rating_counts = self.ratings.rating.value_counts()
         try:
-            task_completion = self.items["completed"].value_counts(normalize=True)[True] * 100
-            self.overall_completion.setText(
-                f"{round(task_completion, 2)}%"
-                )
+            task_completion = (
+                self.items["completed"].value_counts(normalize=True)[True] * 100
+            )
+            self.overall_completion.setText(f"{round(task_completion, 2)}%")
         except KeyError:
-            self.overall_completion.setText('0%')
+            self.overall_completion.setText("0%")
 
     def display_task_stats(self):
         # NOTE there has to be a better way to do this
-        tasks = self.items[self.items.item_type==0]
+        tasks = self.items[self.items.item_type == 0]
         task_counts = tasks.item_name.value_counts()
         self.most_common.setText(task_counts.index[0])
 
         comp_rates = tasks.groupby("item_name").completed.value_counts(normalize=True)
         try:
-            comp_rates_true = comp_rates[comp_rates.index.get_level_values('completed').isin([True])].sort_values()
+            comp_rates_true = comp_rates[
+                comp_rates.index.get_level_values("completed").isin([True])
+            ].sort_values()
 
-            worst_name, worst_rate = comp_rates_true.head(1).index[0][0], comp_rates_true.head(1)[0]
-            best_name, best_rate = comp_rates_true.tail(1).index[0][0], comp_rates_true.tail(1)[0]
+            worst_name, worst_rate = (
+                comp_rates_true.head(1).index[0][0],
+                comp_rates_true.head(1)[0],
+            )
+            best_name, best_rate = (
+                comp_rates_true.tail(1).index[0][0],
+                comp_rates_true.tail(1)[0],
+            )
 
             self.most_completed.setText(f"{best_name} ({round(best_rate * 100)}%)")
             self.least_completed.setText(f"{worst_name} ({round(worst_rate * 100)}%)")
@@ -148,6 +174,7 @@ class StatsView(QWidget):
         self.get_data()
         self.display_stats()
 
+
 def main():
     if len(sys.argv) > 1:
         db = f"tests/{sys.argv[1]}"
@@ -161,8 +188,5 @@ def main():
     app.exec_()
 
 
-
 if __name__ == "__main__":
     main()
-
-
